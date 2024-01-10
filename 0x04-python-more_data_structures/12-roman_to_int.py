338ca21b088380dd98e974ce97be3ad50a0aec38
Bonolo <easytmashabela@gmail.com>
2024-01-10 12:30:36 -0800

    Py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
+def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
 
 
 def roman_to_int(roman_string):
-    if not _roman_string:
+    if not roman_string:
         return 0
 
     if not isinstance(roman_string, str):
@@ -25,16 +25,16 @@ def roman_to_int(roman_string):
     list_num = [0]
 
     for ch in roman_string:
-        for r_num inlist_keys:
+        for r_num in list_keys:
             if r_num == ch:
                 if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
                     list_num = [rom_n.get(ch)]
                 else:
-                    list_num.append(rom_nget(ch))
+                    list_num.append(rom_n.get(ch))
 
                 last_rom = rom_n.get(ch)
 
-    num += to subtract(list_num)
+    num += to_subtract(list_num)
 
     return (num)
