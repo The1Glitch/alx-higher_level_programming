682bb909b2134ede83b183e6cea776055b8623de
Bonolo <easytmashabela@gmail.com>
2024-01-30 10:08:26 -0800

    Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """"Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+       TypeError: if either of 
