3c57a04bff08481ed928c7b08030f99721e6c4d3
Bonolo <easytmashabela@gmail.com>
2024-02-07 06:10:28 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     expect FileNotFoundError:
         item = []
-        item.extend(sys.argv[1:])
-        save_to_json_file(items, "add_item.json")
+    item.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
