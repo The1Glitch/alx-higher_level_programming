7a94531345341a0ce5b5d26f2d871cb206ea0748
The1Glitch <easytmashabela@gamil.com>
2024-04-18 15:25:59 -0700

    Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
