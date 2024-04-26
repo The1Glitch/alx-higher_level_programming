7517a713e469a50df1da0ff3701c4d02cc2d3b6e
The1Glitch <easytmashabela@gamil.com>
2024-04-26 15:31:08 -0700

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Script that takes 2 arguments in order to solve the given challenge
+Python script that takes 2 arguments in order to solve the given challenge
 """
 import requests
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == '__main':
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     r = requests.get(url)
-    commit = r.json()
+    commits = r.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
